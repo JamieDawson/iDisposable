072906d069941d653651fa312124220b0a452f2e
Jamie Robert Dawson <jamierobertdawson@gmail.com>
2016-01-26 20:35:25 -0500

    iDisposable

---
+++ IDisposable test/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub btnMessage_Click(sender As Object, e As EventArgs) Handles btnMessage.Click
+        Using myDoStuff As New doStuff
+            myDoStuff.ShowMoreMessages()
+            myDoStuff.showMessage()
+
+        End Using
+    End Sub
+End Class
---
+++ IDisposable test/doStuff.vb
@@ -0,0 +1,48 @@
+﻿Public Class doStuff
+    Implements IDisposable
+
+
+#Region "IDisposable Support"
+    Private disposedValue As Boolean ' To detect redundant calls
+
+    ' IDisposable
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                ' TODO: dispose managed state (managed objects).
+            End If
+
+            ' TODO: free unmanaged resources (unmanaged objects) and override Finalize() below.
+            ' TODO: set large fields to null.
+        End If
+        disposedValue = True
+    End Sub
+
+    ' TODO: override Finalize() only if Dispose(disposing As Boolean) above has code to free unmanaged resources.
+    'Protected Overrides Sub Finalize()
+    '    ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+    '    Dispose(False)
+    '    MyBase.Finalize()
+    'End Sub
+
+    ' This code added by Visual Basic to correctly implement the disposable pattern.
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+        Dispose(True)
+        ' TODO: uncomment the following line if Finalize() is overridden above.
+        ' GC.SuppressFinalize(Me)
+    End Sub
+#End Region
+
+
+    Public Sub showMessage()
+        MsgBox("This is a Message 4")
+    End Sub
+
+    Public Sub ShowMoreMessages()
+        MsgBox("This is a message 1")
+        MsgBox("This is a message 2")
+        MsgBox("This is a message 3")
+    End Sub
+
+End Class
